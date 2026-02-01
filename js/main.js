af169c7fa52bed023827a984ceddafd593eb3fde
Jing Liu <jintonic@physino.xyz>
2026-01-31 22:21:07 -0600

    enabled rendering of the scene

--- js/main.js
+++ js/main.js
@@ -1,32 +1,46 @@
-import * as THREE from 'three';
-
 import { Editor } from 'three/editor/js/Editor.js';
-import { Config } from 'three/editor/js/Config.js';
+const editor = new Editor();
+window.editor = editor;
+
+console.log('create editor UI');
+
 import { Viewport } from 'three/editor/js/Viewport.js';
+import { Menubar } from 'three/editor/js/Menubar.js';
 import { Toolbar } from 'three/editor/js/Toolbar.js';
 import { Sidebar } from 'three/editor/js/Sidebar.js';
-import { Menubar } from 'three/editor/js/Menubar.js';
-import { Player } from 'three/editor/js/Player.js';
 import { Resizer } from 'three/editor/js/Resizer.js';
 
-const config = new Config();
-const editor = new Editor();
-editor.config = config;
-window.editor = editor; // ViewHelper often looks for window.editor!
-window.THREE = THREE;
-
 const viewport = new Viewport(editor);
+const menubar = new Menubar(editor);
 const toolbar = new Toolbar(editor);
 const sidebar = new Sidebar(editor);
-const menubar = new Menubar(editor);
-const player = new Player(editor);
 const resizer = new Resizer(editor);
 
+// keep everything above viewport
 document.body.appendChild(viewport.dom);
+document.body.appendChild(menubar.dom);
 document.body.appendChild(toolbar.dom);
 document.body.appendChild(sidebar.dom);
-document.body.appendChild(menubar.dom);
-document.body.appendChild(player.dom);
 document.body.appendChild(resizer.dom);
 
-console.log('Modern Editor Initialized');
+import * as THREE from 'three';
+window.THREE = THREE;
+// create a renderer to render the scene
+editor.signals.rendererCreated.dispatch(
+  new THREE.WebGLRenderer({ antialias: true })
+);
+// trigger re-rendering upon window resize
+window.addEventListener('resize', () => editor.signals.windowResize.dispatch());
+editor.signals.windowResize.dispatch();
+
+console.log('editor UI created');
+
+console.log('enable auto save');
+import { autoSave } from './Storage.js';
+autoSave(editor);
+console.log('auto save enabled');
+
+console.log('enable file drop');
+import { FileDropHandler } from './FileDropHandler.js';
+new FileDropHandler(editor);
+console.log('file drop enabled');
